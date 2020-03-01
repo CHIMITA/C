9ad2479d8ecd5296ee3ac2ac16a56c83c3424558
Park Ji Won <smile0375-@naver.com>
2020-03-01 22:32:01 +0900

    Basic

---
+++ Operator/opreator.c/variable.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+	int a = 10;
+	int b = 3;
+
+	float c = 1.5;
+	float d = 2.5;
+
+	printf("µ¡¼À : ");
+	printf("a + b = %d\n", a + b);
+
+	printf("»¬¼À : ");
+	printf("a - b = %d\n", a - b);
+	printf("b - a = %d\n", b - a);
+
+	printf("°ö¼À : ");
+	printf("c * d = %f\n", c * d);
+
+	printf("³ª´°¼À : ");
+	printf("c / d = %f\n", c / d);
+
+	printf("³ª¸ÓÁö : ");
+	printf("a %% b = %d\n", a % b);
+
+	return 0;
+}
